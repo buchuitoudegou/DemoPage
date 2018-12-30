cfc0c50ef2510c4b86b9743ba5a06c7062263180
unknown <756541536@qq.com>
2018-12-30 14:12:11 +0800

    initial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <div id="overview" class="content">
-    <el-carousel class="carousel" height="100%" indicator-position="none" :interval="9000">
+    <el-carousel class="carousel"  height="100%" :interval="9000">
       <el-carousel-item class="img-wrapper" v-for="image in images" :key="image.key" :name="image.key">
         <img :class="`card-${image.key} card`" :src="image.imageUrl"/>
       </el-carousel-item>
@@ -10,9 +10,9 @@
     color: #ffffff; background-color: #a8071a">explore</div>
   </div>
   <div class="content-inner" style="position: absolute; z-index: 1; 
-  margin: auto; left:0;top:0;bottom:0;right:0; display: flex; justify-content: center; align-items: center;">
-    <p class="description">
-      <span>Itune.</span>
+  margin: auto; left:0;top:20vh;bottom:20vh;right:0; display: flex; justify-content: center; align-items: center;">
+    <p class="description"  style="color: white; font-size: 3rem;">
+      <span style="font-size: 5rem;">Itune.</span>
       <br>Your music, movies, and TV shows <br>take center stage.</p>
   </div>
 </div>
@@ -48,5 +48,9 @@ export default class Home extends Vue {
 .card {
   height: 100%;
   width: 100%;
+}
+#overview li.el-carousel__indicator {
+  position: relative;
+  z-index: 5;
 }
 </style>
